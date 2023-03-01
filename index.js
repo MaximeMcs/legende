741f53f8817b61ec80bf4d288294a6e0be372cee
MARCUCCILLI Maxime <maxime.marcuccilli@soprasteria.com>
2023-03-01 11:10:59 +0100

    border fusion

--- index.js
+++ index.js
@@ -9,25 +9,45 @@ const generateTable = (mapStyle) => {
     let opacity = "";
     let dasharray = "";
     let width = "";
+    let id = layer.id.split('-')[1];
 
     switch (layer.type) {
       case "symbol":
         if (layer.layout["icon-image"]) {
-         row = `<tr><td style="text-align: center;"><img src='_svg/${layer.layout["icon-image"]}.svg' alt="icon not found" /></td><td>${layer.id}</td><td>${layer.type}</td></tr>`;
+         row = `<tr><td style="text-align: center;"><img src='_svg/${layer.layout["icon-image"]}.svg' alt="icon not found" /></td><td>${id}</td><td>${layer.type}</td></tr>`;
         }
          break;
-      case "background":
       case "fill":
         color = paint["background-color"] || paint["fill-color"];
         opacity = paint["fill-opacity"] || "1";
-        row = `<tr><td><div style="margin: 0 auto; width: 40px; height: 25px; background-color: ${color}; opacity: ${opacity}"></div></td><td>${layer.id}</td><td>${layer.type}</td></tr>`;
+        row = `<tr><td><div style="margin: 0 auto; width: 40px; height: 25px; background-color: ${color}; opacity: ${opacity}"></div></td><td>${id}</td><td>${layer.type}</td></tr>`;
         break;
       case "line":
+        if (mapStyle.layers[mapStyle.layers.indexOf(layer) - 1].id.endsWith("border")) {
+          break;
+        }
         color = paint["line-color"];
         opacity = paint["line-opacity"] || "1";
         dasharray = paint["line-dasharray"] || "0";
         width = paint["line-width"];
-        row = `<tr><td style="text-align: center;"><svg style="height: 40px; width: 40px;" viewBox="0 0 100 100"><line x1="0" y1="50" x2="100" y2="50" stroke="${color}" stroke-width="${width}" stroke-dasharray="${dasharray}" stroke-opacity="${opacity}" /></svg></td><td>${layer.id}</td><td>${layer.type}</td></tr>`;
+        if (layer.id.endsWith("-border")) {
+          const nextLayer = mapStyle.layers[mapStyle.layers.indexOf(layer) + 1];
+          if (nextLayer && nextLayer.type === "line" && id === nextLayer.id.split('-')[1]) {
+            midId = nextLayer.id.split('-')[1];
+            midColor = nextLayer.paint["line-color"];
+            midOpacity = nextLayer.paint["line-opacity"] || "1";
+            midDasharray = nextLayer.paint["line-dasharray"] || "0";
+            midWidth = nextLayer.paint["line-width"];    
+            row = `<tr><td style="text-align: center;"><svg style="height: 40px; width: 40px; background-color: rgba(224, 224, 224, 1);" viewBox="0 0 100 100">
+                  <line x1="0" y1="30" x2="100" y2="30" stroke="${color}" stroke-width="10px" stroke-dasharray="${dasharray}" stroke-opacity="${opacity}" />
+                  <line x1="0" y1="40" x2="100" y2="40" stroke="${midColor}" stroke-width="10px" stroke-dasharray="${midDasharray}" stroke-opacity="${midOpacity}" />
+                  <line x1="0" y1="50" x2="100" y2="50" stroke="${color}" stroke-width="10px" stroke-dasharray="${dasharray}" stroke-opacity="${opacity}" />
+                  </svg></td><td>${midId}</td><td>${nextLayer.type}</td></tr>`;
+            console.log();
+            break;
+          }
+        }    
+        row = `<tr><td style="text-align: center;"><svg style="height: 40px; width: 40px; background-color: rgba(224, 224, 224, 1);" viewBox="0 0 100 100"><line x1="0" y1="50" x2="100" y2="50" stroke="${color}" stroke-width="10px" stroke-dasharray="${dasharray}" stroke-opacity="${opacity}" /></svg></td><td>${id}</td><td>${layer.type}</td></tr>`;
         break;
       default:
         break;
